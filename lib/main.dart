e9cf94313ce5ed28ac86c77cca13986d8f790fab
sbn <sopaknin@foximail.com>
2022-04-04 12:11:31 +0800

    放彩带，数量减少

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Home(),
         debugShowCheckedModeBanner: false,
-        theme: ThemeData.light()
-    );
+        theme: ThemeData.light());
   }
 }
 
@@ -22,7 +21,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   late ConfettiController controller1;
 
   @override
@@ -31,91 +29,91 @@ class _HomeState extends State<Home> {
     super.initState();
   }
 
-  @override
-  void dispose() {
-    controller1.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   controller1.dispose();
+  //   super.dispose();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: ConfettiWidget(
-                    confettiController: controller1,
-                    shouldLoop: true,
-                    blastDirection: pi / 2,
-                    gravity: 0.02,
-                    numberOfParticles: 100,
-                    emissionFrequency: 0.02,
-                    displayTarget: true,
-                    blastDirectionality: BlastDirectionality.explosive,
-                    colors: [
-                      Colors.redAccent,
-                      Colors.purpleAccent,
-                      Colors.pinkAccent,Colors.orange,
-                      Colors.orangeAccent
-                    ],
-                  ),
-                ),
-                // ignore: deprecated_member_use
-                SizedBox(height: 100,),
-                ElevatedButton(
-                    onPressed: (){
-                      setState(() {
-                        controller1.play();
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context){
-                              return AlertDialog(
-                                content: Container(
-                                  height:100,
-                                  width:100,
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      border: Border.all(width: 4,color: Colors.redAccent),
-                                      borderRadius: BorderRadius.circular(25),
-                                      boxShadow: [
-                                        BoxShadow(
-                                            blurRadius: 25,
-                                            color: Colors.redAccent,
-                                            offset: Offset(1,15)
-                                        )
-                                      ]
-                                  ),
-                                  child: Text("HAPPY BIRTHDAY",
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontStyle: FontStyle.italic,
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 25
-                                    ),),
-                                ),
-                              );
-                            });
-
-                      });
-                    },
-
-                    child: Text("Press",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 25,
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.w800
-                      ),)
-                )
-              ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: ConfettiWidget(
+                confettiController: controller1,
+                shouldLoop: true,
+                blastDirection: pi / 2,
+                gravity: 0.02,
+                numberOfParticles: 50,
+                emissionFrequency: 0.02,
+                displayTarget: true,
+                blastDirectionality: BlastDirectionality.explosive,
+                colors: [
+                  Colors.redAccent,
+                  Colors.purpleAccent,
+                  Colors.pinkAccent,
+                  Colors.orange,
+                  Colors.orangeAccent
+                ],
+              ),
             ),
-          ),
-        )
-    );
+            // ignore: deprecated_member_use
+            SizedBox(
+              height: 100,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    controller1.play();
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            content: Container(
+                              height: 100,
+                              width: 100,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(
+                                      width: 4, color: Colors.redAccent),
+                                  borderRadius: BorderRadius.circular(25),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        blurRadius: 25,
+                                        // color: Colors.redAccent,
+                                        offset: Offset(1, 15))
+                                  ]),
+                              child: Text(
+                                "HAPPY BIRTHDAY",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    // fontStyle: FontStyle.italic,
+                                    // fontWeight: FontWeight.w700,
+                                    fontSize: 25),
+                              ),
+                            ),
+                          );
+                        });
+                  });
+                },
+                child: Text(
+                  "点击解锁好运",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w800),
+                ))
+          ],
+        ),
+      ),
+    ));
   }
 }
